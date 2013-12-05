6b6564dfbde53cb5a52c6820c9d533411ecb1b41
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-12-05 13:45:05 -0600

    Working sample of multiple invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,15 +9,79 @@ end
 
 
 def create
-# HAS TO BE CHANGED TO FINDING THE RIGHT GAME CORRESPONDING TO THE PLAYER/USER
-@invitation = Invitation.new
 
-@invitation.game_id = params[:game_id]
-@invitation.invite_email = params[:invite_email]
-@invitation.save
+if params[:invite_email_0].present?
+  i_0 = Invitation.new
+  i_0.game_id = params[:game_id]
+  i_0.invite_email = params[:invite_email_0]
+  i_0.save
+end
+
+if params[:invite_email_1].present?
+  i_1 = Invitation.new
+  i_1.game_id = params[:game_id]
+  i_1.invite_email = params[:invite_email_1]
+  i_1.save
+end
+
+if params[:invite_email_2].present?
+  i_2 = Invitation.new
+  i_2.game_id = params[:game_id]
+  i_2.invite_email = params[:invite_email_2]
+  i_2.save
+end
+
+if params[:invite_email_3].present?
+  i_3 = Invitation.new
+  i_3.game_id = params[:game_id]
+  i_3.invite_email = params[:invite_email_3]
+  i_3.save
+end
+
+if params[:invite_email_4].present?
+  i_4 = Invitation.new
+  i_4.game_id = params[:game_id]
+  i_4.invite_email = params[:invite_email_4]
+  i_4.save
+end
+
+if params[:invite_email_5].present?
+  i_5 = Invitation.new
+  i_5.game_id = params[:game_id]
+  i_5.invite_email = params[:invite_email_5]
+  i_5.save
+end
+
+redirect_to user_url(session[:u_id])
 
 
 end
+
+
+
+
+
+
+
+
+
+# HAS TO BE CHANGED TO FINDING THE RIGHT GAME CORRESPONDING TO THE PLAYER/USER
+# @invitation = Invitation.new
+
+# @invitation.game_id = params[:game_id]
+# @invitation.invite_email = params[:invite_email]
+
+
+
+# if @invitation.save
+#       redirect_to user_url(session[:u_id])
+#    else
+#       # @errors = @t.errors.full_messages
+#       # render 'new'
+#   end
+
+
+# end
 
 
 def group_invitation
