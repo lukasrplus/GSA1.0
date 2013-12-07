7cc66d599b9ab5d3d0f6e50cc253e956dfdc548c
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-12-07 13:38:57 -0600

    Added Heroku Email Credentials

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,18 @@ require File.expand_path('../application', __FILE__)
 GSA10::Application.initialize!
 
 
+if Rails.env.development?
 CREDENTIALS = YAML.load_file(Rails.root.join('config','email.yml'))
+end
+
+
+GSA_EMAIL = Rails.env.development? ? CREDENTIALS['username'] : ENV['GSA_EMAIL']
+GSA_PASSWORD = Rails.env.development? ? CREDENTIALS['password'] : ENV['GSA_PASSWORD']
+
 
 ActionMailer::Base.smtp_settings = {
-  user_name: CREDENTIALS['username'],
-  password: CREDENTIALS['password'],
+  user_name: GSA_EMAIL,
+  password: GSA_PASSWORD,
   address: 'smtp.gmail.com',
   port: 587,
   authentication: 'plain',
